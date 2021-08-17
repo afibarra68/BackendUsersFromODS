89bf3593c2b875fe6e02cd226e7ea16d14932732
zfarmingdev1 <zfarming.dev1@gmail.com>
2021-08-17 11:18:39 -0500

    first-Uploading

---
+++ src/main/java/com/example/UserResting/UserRestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserResting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UserResting/UserRestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserResting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
